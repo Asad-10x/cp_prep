cdc12fc525cba0c97d9b48ec8b7598abc9155886
Asad-Ali <selcouth683@gmail.com>
2024-12-30 20:15:28 +0500

    first topic: datatypes & variables

---
+++ 1_datatypes_variables.cpp
@@ -0,0 +1,34 @@
+#include <iostream> // stands for Input/Output Stream, used to take input and show output 
+#include <climits> // to get Maximum limits of certain datatypes.
+using namespace std;
+
+int main( ){
+  /*
+  integer: used for storing numerical values.
+  long int: used for string 32-bit integers. 
+  float: used for decimal values.
+  double: used for high precision decimal values.
+  bool: used for 'true' or 'false' values.
+  char: used for alphabets e.g. a, b, c, ..x.
+  string: used for combination of alphabets. e.g. cat, dog, sentences, 'he is going', etc.
+  enums: used for user-defined set of numeric values and refer them by names.
+  */
+
+  // int var; // declaration
+  // var = 12; // initialization
+  int a = 12.05;
+  long int b = 122137637263173;
+  float c = 2.99;
+  double d = 3224342.3445323;
+  bool e = true; // true/1 , false/0
+  char f = 'a';
+  string g = "Mansab"; 
+  enum h {
+    apple, 
+    mango, 
+    banana,
+    grapes
+  };
+
+  // write the cout statements yourself. 
+};
